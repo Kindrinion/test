ea67247ff0af13c77f5f4fcb21e42ea89f6f5e10
아기 <135098835+Kindrinion@users.noreply.github.com>
2023-05-31 15:28:58 +0900

    pxt.json, main.blocks, main.ts 업데이트하기

--- main.ts
+++ main.ts
@@ -1,3 +1,30 @@
-basic.forever(function () {
-	
+input.onLogoEvent(TouchButtonEvent.Pressed, function () {
+    radio.sendString("b")
 })
+input.onButtonPressed(Button.A, function () {
+    radio.sendString("l")
+})
+input.onButtonPressed(Button.AB, function () {
+    radio.sendString("f")
+})
+radio.onReceivedString(function (receivedString) {
+    if (receivedString == "f") {
+        maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 0)
+    } else {
+    	
+    }
+})
+input.onButtonPressed(Button.B, function () {
+    radio.sendString("r")
+})
+input.onGesture(Gesture.LogoUp, function () {
+    radio.sendString("s")
+})
+radio.setGroup(616)
+basic.showLeds(`
+    . # . # .
+    # . # . #
+    # . . . #
+    . # . # .
+    . . # . .
+    `)
